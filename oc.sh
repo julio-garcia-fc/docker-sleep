e570dc80e158c42d566bfe82761bf59bdf80ef0f
Garcia, Julio C <julio.garcia@ca.com>
2017-12-15 16:04:16 -0700

    simple scripting

--- oc.sh
+++ oc.sh
@@ -1,11 +1,15 @@
-#!/bin/bash
+#!/bin/bash -x
 
-#oc cluster up --public-hostname=julio-oc.10.238.41.233.nip.io
+oc cluster up --public-hostname=julio-oc.10.238.41.233.nip.io
 
-# oc new-project julio 
-oc secrets new-sshauth sleep-key-rsa --ssh-privatekey=/home/julio/.ssh/sleep-key_rsa
-# oc secrets link builder sleep-key-rsa
+SSH_URI="git@github-isl-01.ca.com:garju09/docker-sleep.git"
+KEY_NAME=sleep-key-rsa
+KEY_FILE=sleep-key_rsa
+oc secrets new-sshauth $KEY_NAME --ssh-privatekey=./$KEY_FILE
+oc annotate secret/$KEY_NAME "build.openshift.io/source-secret-match-uri-1=ssh://${SSH_URI}"
+oc secrets link builder $KEY_NAME
 
-# oc new-app $PWD --source-secret=sleep-key-rsa
-ssh-add ./sleep-key_rsa
-oc new-app --source-secret=sleep-key-rsa git@github-isl-01.ca.com:garju09/docker-sleep.git
+# oc new-app $PWD --source-secret=$KEY_NAME
+ssh-add ./$KEY_FILE
+oc new-app --source-secret=$KEY_NAME $SSH_URI
+# oc new-app git@github-isl-01.ca.com:garju09/docker-sleep.git
