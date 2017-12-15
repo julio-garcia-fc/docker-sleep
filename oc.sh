09a0a593b42182ae2c0b0f4c6b55efbb4d7d73d7
Garcia, Julio C <julio.garcia@ca.com>
2017-12-15 13:33:48 -0700

    get git secret to work

--- oc.sh
+++ oc.sh
@@ -4,6 +4,8 @@
 
 # oc new-project julio 
 oc secrets new-sshauth sleep-key-rsa --ssh-privatekey=/home/julio/.ssh/sleep-key_rsa
+# oc secrets link builder sleep-key-rsa
 
-oc new-app $PWD --source-secret=sleep-key-rsa
-oc secrets link builder sleep-key-rsa
+# oc new-app $PWD --source-secret=sleep-key-rsa
+ssh-add ./sleep-key_rsa
+oc new-app --source-secret=sleep-key-rsa git@github-isl-01.ca.com:garju09/docker-sleep.git
