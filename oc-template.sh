9453d45f35d7681e3c263a5cd3869b268798d2b9
Garcia, Julio C <julio.garcia@ca.com>
2017-12-18 16:57:42 -0700

    Trigger build and check logs

--- oc-template.sh
+++ oc-template.sh
@@ -2,7 +2,11 @@
 
 # oc cluster up --public-hostname=julio-oc.10.238.41.233.nip.io
 
-IMAGES_TEMPLATE=docker-sleep-image-template
+PREFIX=docker-sleep
+IMAGES_TEMPLATE=${PREFIX}-image-template
 oc create -f $IMAGES_TEMPLATE.json 
 oc process $IMAGES_TEMPLATE | oc create -f -
 
+sleep 5
+oc start-build $PREFIX
+oc logs -f bc/$PREFIX
