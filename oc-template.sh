3ec5bdec690eef9ac47ef49a4820b05c2bc66a12
Garcia, Julio C <julio.garcia@ca.com>
2017-12-20 10:50:14 -0700

    Crete deployment

--- oc-template.sh
+++ oc-template.sh
@@ -3,10 +3,17 @@
 # oc cluster up --public-hostname=julio-oc.10.238.41.233.nip.io
 
 PREFIX=docker-sleep
+
 IMAGES_TEMPLATE=${PREFIX}-image-template
 oc create -f $IMAGES_TEMPLATE.json 
 oc process $IMAGES_TEMPLATE | oc create -f -
 
-sleep 5
+sleep 10
 oc start-build $PREFIX
 oc logs -f bc/$PREFIX
+
+set -x 
+
+INFRA_TEMPLATE=${PREFIX}-infra-template
+oc create -f $INFRA_TEMPLATE.json
+oc process -p DOCKER_REGISTRY=172.30.1.1 $INFRA_TEMPLATE | oc create -f -
