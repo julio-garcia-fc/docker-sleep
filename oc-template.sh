b1395e65647b27d91f5c626040e744372218f9a9
Garcia, Julio C <julio.garcia@ca.com>
2017-12-21 11:51:24 -0700

    add the registry port

--- oc-template.sh
+++ oc-template.sh
@@ -16,7 +16,7 @@ set -x
 
 INFRA_TEMPLATE=${PREFIX}-infra-template
 oc create -f $INFRA_TEMPLATE.json
-oc process -p DOCKER_REGISTRY=172.30.1.1 $INFRA_TEMPLATE | oc create -f -
+oc process -p DOCKER_REGISTRY=172.30.1.1:5000 $INFRA_TEMPLATE | oc create -f -
 sleep 10
 oc describe $PREFIX
 oc rollout latest dc/$PREFIX
